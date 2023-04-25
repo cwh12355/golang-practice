5925536ef789ead36bcc1d8178f4669dd03ddda5
cwh12355 <cwh12355@163.com>
2023-04-25 14:36:43 +0800

    first mofify
    Signed-off-by: cwh12355 <cwh12355@163.com>

--- gofiles/engage/3-variables.go
+++ gofiles/engage/3-variables.go
@@ -5,11 +5,17 @@ the compiler to e.g. check type-correctness of function calls.
 
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("here we go again")
 
+	first := sha256.New()
+	//first.Write([]byte(ses.value))
 	// var declares 1 or more variables.
 	var a string = "This is Andela"
 	fmt.Println(a)
@@ -27,11 +33,29 @@ func main() {
 	// For example, the zero value for an int is 0.
 	var e int
 	fmt.Println(e)
-
+	type session struct {
+		key   int
+		value string
+		name  int
+		cache bool
+	}
+	var (
+		ses session
+		flo string
+	)
+	ses.value = "You are a hash man"
+	flo = "Yes,you are right"
 	// For example, the zero value for a string is ' '.
 	var letter string
+	fmt.Println(ses.value)
 	fmt.Println(letter)
-
+	first.Write([]byte(ses.value))
+	fmt.Println(first.Sum(nil))
+	second := sha512.New()
+	second.Write([]byte(flo))
+	first.Size()
+	second.Size()
+	fmt.Println(second.Sum(nil))
 	// The := syntax is shorthand for declaring and initializing a variable, e.g.
 	// for var f string = "short" in this case.
 	short := "This is for short declaration."
