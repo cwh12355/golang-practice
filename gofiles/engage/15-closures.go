2e7d94d6e489ec67959e07e7d9c61813a499be54
cwh12355 <cwh12355@163.com>
2023-04-27 13:38:58 +0800

    feat
    Signed-off-by: cwh12355 <cwh12355@163.com>

--- gofiles/engage/15-closures.go
+++ gofiles/engage/15-closures.go
@@ -21,13 +21,38 @@ func main() {
 
 	newInts := intSeq()
 	fmt.Println(newInts())
+	seqfunc := seqsun()
+	fmt.Println(seqfunc(256, "cc"))
 }
 
 func intSeq() func() int {
 	i := 0
 	return func() int {
-
 		i++
+
 		return i
 	}
+}
+func seqsun() func(int, string) (error, int, string) {
+	i := 0
+	str := []string{}
+	return func(br1 int, inputstring string) (err error, in int, res string) {
+		str = append(str, "cc1", "cc2", "cc3")
+		i += 12
+		br1 = 12
+		for index, street := range str {
+			if street == "cc" {
+				br1 += 1
+				fmt.Println(index)
+				return nil, br1, street
+				//fmt.Println(index)
+			}
+			if street == "ccr" {
+				i += 1
+				return nil, i, res
+			}
+		}
+		return nil, br1, "You are a six star man"
+	}
+	//return nil, str[1]
 }
